c38cdcb1cee3b3e896a3d8e167357e2f51c360f9
Hariharan-1010 <irahkrats74@gmail.com>
2024-10-09 20:18:18 +0530

    second commit

--- event.js
+++ event.js
@@ -5,9 +5,10 @@ tags.forEach(tag => {
         const cardId = tag.getAttribute('id');
         const cardNum = cardId.substring(9, cardId.length);
         const card = document.getElementById('hoverCard-'+cardNum);
+
+        console.log(card)
         
         const rect = tag.getBoundingClientRect();
-        const containerRect = document.querySelector('#container').getBoundingClientRect();
         card.style.display = 'block';
         let cardTop = rect.bottom + window.scrollY; // Position below the tag
         let cardLeft = rect.left + window.scrollX; // Align with the tag
@@ -16,16 +17,10 @@ tags.forEach(tag => {
         if (cardLeft + card.offsetWidth > containerRect.right) {
             cardLeft = containerRect.right - card.offsetWidth - 10; // Adjust to the left
         }
-        else {
-            cardLeft -= 10;
-        }
 
         // Check if the card overflows the bottom side
         if (cardTop + card.offsetHeight > containerRect.bottom) {
             cardTop = rect.top + window.scrollY - card.offsetHeight - 10; // Position above the tag
-        }
-        else {
-            cardTop -= 100;
         }
 
         card.style.display = 'block';
